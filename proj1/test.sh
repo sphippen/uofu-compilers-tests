9136446e06f602a47a30fa39711f6abf1b225c1a
Spencer Phippen <spencer.phippen@gmail.com>
2015-02-03 19:53:12 -0700

    Updated proj1 test script to test invalid pylex input tests

--- proj1/test.sh
+++ proj1/test.sh
@@ -16,17 +16,28 @@ total=0
 success=0
 for f in ./tests/*.py; do
   total="$(echo "$total+1" | bc)"
-  base="$(basename -- $f)"
-  outfile="./$DIR/${base%.py}.out"
-  "$BIN" "$f" > "$outfile"
-  diffout="${outfile%.out}.diff"
-  diff -- "$outfile" "${f%.py}.out" > "$diffout"
-  if [ $? -ne 0 ] ; then
-    printf -- "\n%s: FAILED (diff shown below):\n" "$base"
-    cat -- "$diffout"
+  expectedout="${f%.py}.out"
+  base="$(basename -- "$f")"
+  if [ -f "$expectedout" ] ; then
+    outfile="./$DIR/${base%.py}.out"
+    "$BIN" "$f" > "$outfile"
+    diffout="${outfile%.out}.diff"
+    diff -- "$outfile" "$expectedout" > "$diffout"
+    if [ $? -ne 0 ] ; then
+      printf -- "\n%s: FAILED (diff shown below):\n" "$base"
+      cat -- "$diffout"
+    else
+      printf -- "\n%s: PASSED\n" "$base"
+      success="$(echo "$success+1" | bc)"
+    fi
   else
-    printf -- "\n%s: PASSED\n" "$base"
-    success="$(echo "$success+1" | bc)"
+    "$BIN" "$f" > /dev/null
+    if [ $? -eq 0 ] ; then
+      printf -- "\n%s: FAILED (should have errored)\n" "$base"
+    else
+      printf -- "\n%s: PASSED\n" "$base"
+      success="$(echo "$success+1" | bc)"
+    fi
   fi
 done
 printf -- "\n%s/%s TESTS PASSED\n" "$success" "$total"
