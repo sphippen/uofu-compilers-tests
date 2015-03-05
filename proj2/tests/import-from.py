73e171ce0e6d40bf4a4977d7cd45b58100d5af7e
Josh Kunz <joshkunz@me.com>
2015-03-04 19:45:33 -0700

    Fixes import-from case to match pysx semantics

--- proj2/tests/import-from.py
+++ proj2/tests/import-from.py
@@ -1,9 +1,8 @@
 from . import *
 from . import a
 from . import a, b, c
-from . import a, b, c,
+from . import a, b, c
 from . import a as foo, b as boo, c as coo
-from . import a as foo, b as boo, c as coo,
 from . import (a as foo, b as boo, c as coo,)
 from . import (a , b , c ,)
 from ..... import a
