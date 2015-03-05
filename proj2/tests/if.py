d8d64a8d451d2f1f52ec964813c5d88261d73ea2
Josh Kunz <joshkunz@me.com>
2015-03-04 19:53:18 -0700

    Adds if case

---
+++ proj2/tests/if.py
@@ -0,0 +1,23 @@
+if a: pass
+elif b: pass
+elif c: pass
+elif d: pass
+else: pass
+
+if a: pass
+
+if a:
+    pass
+
+if a and b:
+    pass
+
+if a and b:
+    pass
+elif b and c:
+    pass
+
+if a and b:
+    pass
+else:
+    c
