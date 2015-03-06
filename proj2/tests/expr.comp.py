e473fa45ca7d1ba43b3fcdac8c6d551fcffa4ba4
Spencer Phippen <spencer.phippen@gmail.com>
2015-03-05 19:03:21 -0700

    Added test for various comprehensions

---
+++ proj2/tests/expr.comp.py
@@ -0,0 +1,4 @@
+[(x,y) for x in foo(4) if x > 0 if (2+3) == 5 for a,b,*c in bar(10) for *d,e,*f in bar(8)]
+((x,y) for x in foo(4) if x > 0 if (2+3) == 5 for a,b,*c in bar(10) for *d,e,*f in bar(8))
+{(x,y) for x in foo(4) if x > 0 if (2+3) == 5 for a,b,*c in bar(10) for *d,e,*f in bar(8)}
+{x:y for x in foo(4) if x > 0 if (2+3) == 5 for a,b,*c in bar(10) for *d,e,*f in bar(8)}
