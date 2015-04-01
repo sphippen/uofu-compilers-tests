185736318987abfc3a4e3f18653da957c199968c
Josh Kunz <joshkunz@me.com>
2015-03-31 18:49:53 -0600

    Makes sure annotations dict is ordered

--- proj3/tests/annotations.py
+++ proj3/tests/annotations.py
@@ -1,3 +1,4 @@
 def fun(a : "ham", b : str, c : int, *star : list, book : tuple = (), **kw : float):
     return a, b, c, star, book, kw
-print(fun.__annotations__)
+
+print(list(sorted(fun.__annotations__.items())))
