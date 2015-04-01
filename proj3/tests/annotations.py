a85a6cb2cc5db559528fef4139e6a9b48a22af76
Josh Kunz <joshkunz@me.com>
2015-03-31 19:05:02 -0600

    Adds result notation to the test

--- proj3/tests/annotations.py
+++ proj3/tests/annotations.py
@@ -1,4 +1,4 @@
-def fun(a : "ham", b : str, c : int, *star : list, book : tuple = (), **kw : float):
+def fun(a : "ham", b : str, c : int, *star : list, book : tuple = (), **kw : float) -> int:
     return a, b, c, star, book, kw
 
 print(list(sorted(fun.__annotations__.items())))
