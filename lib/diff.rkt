f937a23524d540f6310744b7bc77739c820c3e25
Josh Kunz <joshkunz@me.com>
2015-04-25 14:35:11 -0600

    Adds a diff/no-added option

--- lib/diff.rkt
+++ lib/diff.rkt
@@ -2,7 +2,8 @@
 (require "proc.rkt")
 (require "term-colors.rkt")
 
-(provide diff diff/bytes)
+(provide diff diff/bytes
+         diff/no-added diff/bytes/no-added)
 
 (define *diff-args* `( "--unchanged-group-format=%="
                        "--changed-group-format=%<%>"
@@ -20,13 +21,13 @@
   (copy-port p output)
   (close-output-port output)))
 
-(define (diff old/port new/port)
+(define (diff old/port new/port [args *diff-args*])
  (let ([diff-path      (find-executable-path "diff")]
        [old-temp-path  (make-temporary-file *temp-format*)]
        [new-temp-path  (make-temporary-file *temp-format*)])
   (fill-file-with-port old-temp-path old/port)
   (fill-file-with-port new-temp-path new/port)
-  (let* ([args `(,diff-path ,@*diff-args* ,old-temp-path ,new-temp-path)]
+  (let* ([args `(,diff-path ,@args ,old-temp-path ,new-temp-path)]
          [proc  (apply run args)])
    (begin0
     (if (= (hash-ref proc 'status) 2) ; diff exits with 2 on error
@@ -34,6 +35,17 @@
         (hash-ref proc 'stdout))
     (delete-file old-temp-path)
     (delete-file new-temp-path)))))
+
+(define (diff/no-added old/port new/port)
+ (diff old/port new/port 
+      `( ,(list-ref *diff-args* 0)
+         "--changed-group-format=%>"
+         ,(format "--unchanged-line-format=~a %l\n" (color "\u2713" 'green))
+         ,(format "--new-line-format=~a %l\n" (color "x" 'red)))))
+
+(define (diff/bytes/no-added old/bytes new/bytes)
+ (diff/no-added (open-input-bytes old/bytes)
+                (open-input-bytes new/bytes)))
 
 (define (diff/bytes old/bytes new/bytes)
  (diff (open-input-bytes old/bytes)
