70e5bd269bb10654a104fad7a946841b89f881a1
Josh Kunz <joshkunz@me.com>
2015-04-27 23:21:05 -0600

    Adds extensions to (run) command

--- lib/proc.rkt
+++ lib/proc.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(require "error.rkt")
 (provide run build rm fix-path)
 
 (define (fix-path p) 
@@ -9,7 +10,7 @@
 ; containing the status code, stdout, stderr, binary name, and list of
 ; arguments used to invoke the binary. If supplied, the ouput-poirnt given by
 ; #:input is sent to the spawned processes stdin.
-(define (run binary #:input [input-port null] . flags)
+(define (run binary #:input [input-port null] #:timeout [timeout (void)] . flags)
  ; Run the program as a subprocess
  (let-values ([(proc proc-stdout proc-stdin proc-stderr)
                (apply subprocess (list* #f #f #f binary flags))])
@@ -17,24 +18,26 @@
    (with-handlers ([exn:fail:filesystem? (thunk* (void))])
     (copy-port input-port proc-stdin)))
   (close-output-port proc-stdin)
-  (let ([proc-output (port->bytes proc-stdout)]
-        [proc-error (port->bytes proc-stderr)])
-   (subprocess-wait proc)
-   (begin0
-    (make-immutable-hash `(
-     (status . ,(subprocess-status proc))
-     (stdout . ,proc-output)
-     (stderr . ,proc-error)
-     (binary . ,binary)
-     (args   . ,flags)
-     )
-    )
-    (close-input-port proc-stdout)
-    (close-input-port proc-stderr)
-   )
-  )
- )
-)
+  (let ([collect (lambda ()
+         (let ([proc-output (port->bytes proc-stdout)]
+               [proc-error (port->bytes proc-stderr)])
+          (subprocess-wait proc)
+          (begin0
+           (make-immutable-hash `(
+            (status . ,(subprocess-status proc))
+            (stdout . ,proc-output)
+            (stderr . ,proc-error)
+            (binary . ,binary)
+            (args   . ,flags)
+            )
+           )
+           (close-input-port proc-stdout)
+           (close-input-port proc-stderr))))])
+   (if (not (void? timeout))
+       (with-handlers ([exn:fail:timeout? (lambda (e) (subprocess-kill proc)
+                                                      (raise e))])
+        (begin/timeout timeout (collect)))
+       (collect)))))
 
 ; Writes the output of all given sequences `seqs` to 'to-file'.
 (define (build to-file . seqs)
