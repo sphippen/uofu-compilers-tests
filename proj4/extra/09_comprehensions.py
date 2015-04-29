bedcc8dbab74f77710ddd722413bda40d220839c
Josh Kunz <joshkunz@me.com>
2015-04-29 15:13:32 -0600

    Fixes comprehensions test case

--- proj4/extra/09_comprehensions.py
+++ proj4/extra/09_comprehensions.py
@@ -1,3 +1,3 @@
-print([a for a in range(10) if y > 3])
-print({a for a in range(10) if y % 2})
-print(list(sorted(tuple({a : b for a in range(10) if a * 2 < 15 for b in range(3) if b % 2}))))
+print([a for a in range(10) if a > 3])
+print({a for a in range(10) if a % 2})
+print(list(sorted(tuple({a : b for a in range(10) if a * 2 < 15 for b in range(3) if b % 2}.items()))))
